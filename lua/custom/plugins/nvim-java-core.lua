10d78da22a83eab892dabd8e60bd5f8fce63d9c2
logrusx <jorodrbr@yahoo.com>
2025-07-30 17:12:50 +0300

    make nvim-javause logrusx foks

---
+++ lua/custom/plugins/nvim-java-core.lua
@@ -0,0 +1,7 @@
+local enabled = true
+if not enabled then return {} end
+return {
+    name="nvim-java-core",
+    url = "https://github.com/logrusx/nvim-java-core",
+    branch = "migrate-to-official-mason-repo"
+}
---
+++ lua/custom/plugins/nvim-java.lua
@@ -0,0 +1,7 @@
+local enabled = true
+if not enabled then return {} end
+return {
+    name="nvim-java",
+    url="https://github.com/logrusx/nvim-java",
+    branch = "use-latest-versions-available-in-mason-registry"
+}
